d1632229f152f4434d738d4ecc86921bd1af0254
Gazz-w <105929513+Gazz-w@users.noreply.github.com>
2024-03-20 08:54:02 -0300

    commit do projeto em 50%

---
+++ app.py
@@ -0,0 +1,78 @@
+from flask import Flask, render_template, request, Response
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+from time import sleep
+from helpers import *
+from selecionar_persona import *
+
+load_dotenv()
+
+cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+modelo = "gpt-4"
+
+app = Flask(__name__)
+app.secret_key = 'alura'
+
+contexto = carrega("dados/ecomart.txt")
+
+
+def bot(prompt):
+    maximo_tentativas = 1
+    repeticao = 0
+    personalidade = personas[selecionar_persona(prompt)]
+
+    while True:
+        try:
+            prompt_do_sistema = f'''Você é um chatbot de atendimento a clientes de um e-commerce.
+            Você não deve responder perguntars que não sejam dados do ecomerce informado!
+
+            Você deve gerar respostas utilizando o contexto abaixo
+            Você deve adotar a persona abaixo.
+            # Contexto
+            {contexto}
+
+            #Persona
+            {personalidade}
+            '''
+            response = cliente.chat.completions.create(
+                messages=[
+                    {
+                        "role": "system",
+                        "content": prompt_do_sistema
+                    },
+                    {
+                        "role": "user",
+                        "content": prompt
+                    }
+                ],
+                temperature=1,
+                max_tokens=1000,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0,
+                model=modelo)
+            return response
+        except Exception as erro:
+            repeticao += 1
+            if repeticao >= maximo_tentativas:
+                return "Erro no GPT: %s" % erro
+            print('erro de comunicação com OpenAI:', erro)
+            sleep(1)
+
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    prompt = request.json["msg"]
+    resposta = bot(prompt)
+    texto_resposta = resposta.choices[0].message.content
+    return texto_resposta
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
